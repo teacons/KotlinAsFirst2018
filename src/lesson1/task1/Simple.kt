1767ddb0e5cc102ab8ae703f12b76662a0338ad5
teacons <ivkin91@gmail.com>
2018-09-25 20:53:02 +0300

    Lesson 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -69,10 +69,9 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
-    val inMeters = (sagenes * 48 + arshins * 16 + vershoks) * 0.04445
-    return inMeters
-}
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+        (sagenes * 48 + arshins * 16 + vershoks) * 0.04445
+
 
 /**
  * Тривиальная
@@ -88,10 +87,9 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad * 3600 + min * 
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
-    val length = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
-    return length
-}
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+        sqrt(sqr(x2 - x1) + sqr(y2 - y1))
+
 
 /**
  * Простая
@@ -108,10 +106,8 @@ fun thirdDigit(number: Int): Int = number / 100 % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
-    val timeInTravel = hoursArrive * 60 + minutesArrive - (hoursDepart * 60 + minutesDepart)
-    return timeInTravel
-}
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        hoursArrive * 60 + minutesArrive - (hoursDepart * 60 + minutesDepart)
 
 /**
  * Простая

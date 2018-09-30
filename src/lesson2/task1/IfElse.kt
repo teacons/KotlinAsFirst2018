6d82808c6ca5197d337780acafdc0c590208f54f
teacons <ivkin91@gmail.com>
2018-10-01 00:13:36 +0300

    Lesson 2 edited

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -102,8 +102,8 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
                        rookX2: Int, rookY2: Int): Int = when {
     kingX != rookX1 && kingX != rookX2 && kingY != rookY1 && kingY != rookY2 -> 0
-    kingX == rookX1 || kingY == rookY1 && kingX != rookX2 && kingY != rookY2 -> 1
-    kingX != rookX1 && kingY != rookY1 && kingX == rookX2 || kingY == rookY2 -> 2
+    (kingX == rookX1 || kingY == rookY1) && kingX != rookX2 && kingY != rookY2 -> 1
+    kingX != rookX1 && kingY != rookY1 && (kingX == rookX2 || kingY == rookY2) -> 2
     else -> 3
 }
 
